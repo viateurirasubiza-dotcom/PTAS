33c255b27b2f1cc9f316ba0e9adb769f0cc5464e
viateurirasubiza-dotcom <viateurirasubiza@gmail.com>
2026-06-18 11:10:59 +0200

    Add initial HTML structure for Parent Teacher System

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Parent Teacher System</title>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<style>
+
+body{
+background:#f4f6f9;
+}
+
+.hero{
+height:100vh;
+display:flex;
+justify-content:center;
+align-items:center;
+flex-direction:column;
+}
+
+</style>
+
+</head>
+<body>
+
+<div class="hero">
+
+<h1>Parent Teacher Attendance System</h1>
+
+<p>
+Monitor student attendance between
+teachers and parents.
+</p>
+
+<a href="login.php" class="btn btn-primary">
+Login
+</a>
+
+</div>
+
+</body>
+</html>
